a8184e54c45a5fd17934596f3bbc252294106301
Janakkumar Bhanushali <jbhanush@ws.cs.ryerson.ca>
2018-03-20 20:33:55 -0400

    did some more work on accounts tabs

--- MembershipManagementMachine/src/pc_user_interface/TreasurerUI.java
+++ MembershipManagementMachine/src/pc_user_interface/TreasurerUI.java
@@ -11,18 +11,16 @@ public class TreasurerUI {
 	
 	private String		userName;
 	
-	private JMenuBar 	menuBar;
+	private JMenuBar 	accountsMenuBar;
 	private JMenu		fileMenu;
 	private JMenuItem	open, saveAs, save, exit;
 	private JMenu		helpMenu;
 	private JMenuItem	about, content, documentation, support, checkForUpdates;
 	
-	private JPanel		introPanel;
-	private JLabel		nameLabel;
-	
-	private JPanel		tabsPanel;
 	private JTabbedPane	tabbedPane;
-	private JPanel		accountsTab, customersTab, managementTab;
+	private JPanel		accountsTab;
+	private JTextArea	accountsTextArea;
+	private JPanel		managementTab;
 	
 	public TreasurerUI(String userName) {
 		this.userName = userName;
@@ -35,30 +33,22 @@ public class TreasurerUI {
 	}
 	
 	public void fillFrame() {
-		frame.setJMenuBar(menuBar());
-		frame.add(introPanel(), BorderLayout.NORTH);
-		frame.add(tabsPanel(), BorderLayout.CENTER);
-	}
-	
-	public JPanel tabsPanel() {
-		tabsPanel = new JPanel(new BorderLayout());
-		
-		tabsPanel.add(tabbedPane(), BorderLayout.CENTER);
-		
-		return tabsPanel;
+		frame.setJMenuBar(accountsMenuBar());
+		frame.add(introPanel(),	BorderLayout.NORTH);
+		frame.add(tabbedPane(),	BorderLayout.CENTER);
 	}
 	
 	public JTabbedPane tabbedPane() {
 		tabbedPane = new JTabbedPane();
 		
-		tabbedPane.addTab("Management", null, managementTab(), null);
-		tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+		tabbedPane.addTab("Management", null, managementTab(),	null);
+		tabbedPane.addTab("Accounts",	null, accountsTab(),	null);
 		
-		tabbedPane.addTab("Customers", null, customersTab(), null);
+		tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
 		tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
 		
-		tabbedPane.addTab("Accounts", null, accountsTab(), null);
-		tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
+		// this line allows u to change the tab index to focus on what ever tab u want
+		tabbedPane.setSelectedIndex(1);
 		
 		return tabbedPane;
 	}
@@ -71,44 +61,46 @@ public class TreasurerUI {
 		return managementTab;
 	}
 	
-	public JPanel customersTab() {
-		customersTab = new JPanel();
-		
-		return customersTab;
-	}
-	
 	public JPanel accountsTab() {
-		accountsTab = new JPanel(new BorderLayout());
+		accountsTab = new JPanel(new GridLayout(1,2));
+		
+		JPanel accountsTextPanel = 	new JPanel(new BorderLayout());
+		accountsTextArea = 			new JTextArea();
+		JPanel toolsPanel = 		new JPanel(new GridLayout());
+		
+		accountsTextPanel.setBorder(BorderFactory.createEtchedBorder());
+		accountsTextPanel.add(accountsTextArea, BorderLayout.CENTER);
+		
+		accountsTab.add(toolsPanel);
+		accountsTab.add(accountsTextPanel);
 		
 		return accountsTab;
 	}
 	
 	public JPanel introPanel() {
-		introPanel = new JPanel(new BorderLayout());
+		JPanel introPanel = new JPanel(new BorderLayout());
 		
 		introPanel.setLayout(new GridBagLayout());
 		
-		nameLabel = new JLabel(userName + " signed in as Treasurer");
-		
-		introPanel.add(nameLabel);
+		introPanel.add(new JLabel(userName + " signed in as Treasurer"));
 		
 		return introPanel;
 	}
 	
-	public JMenuBar menuBar() {
-		menuBar = new JMenuBar();
+	public JMenuBar accountsMenuBar() {
+		accountsMenuBar = new JMenuBar();
 		
-		menuBar.add(fileMenu());
-		menuBar.add(helpMenu());
+		accountsMenuBar.add(fileMenu());
+		accountsMenuBar.add(helpMenu());
 		
-		return menuBar;
+		return accountsMenuBar;
 	}
 	
 	public JMenu fileMenu() {
 		fileMenu = new JMenu("File");
 		
 		open = 		new JMenuItem("open");
-		saveAs = 	new JMenuItem("save as");
+		saveAs =	new JMenuItem("save as");
 		save = 		new JMenuItem("save");
 		exit = 		new JMenuItem("exit");
 		
@@ -121,7 +113,7 @@ public class TreasurerUI {
 	}
 	
 	public JMenu helpMenu() {
-		JMenu helpMenu = new JMenu("Help");
+		helpMenu = new JMenu("Help");
 		
 		about = 			new JMenuItem("about");
 		content = 			new JMenuItem("content");
